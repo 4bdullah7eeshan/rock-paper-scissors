012bb650bcea9b06acb7badb9c70068003976cf5
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-08-08 10:51:38 +0530

    feat(js): implement logic for computer choice
    - Used Math.random() to generate numbers 0, 1, and 2, corresponding to rock, paper, and scissors choices.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("hello, world!");
+function getRandomInteger(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    let generator;
+    generator = getRandomInteger(3);
+    if (generator == 0) {
+        return "rock";
+    } else if (generator == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
